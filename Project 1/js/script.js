c76ac972bba7f0b5e1e7df5056a968490ff0f908
Jeremy Gutzwiller <37252393+JeremyGutzy@users.noreply.github.com>
2019-04-05 12:26:47 -0500

    Final Version

--- Project 1/js/script.js
+++ Project 1/js/script.js
@@ -1,19 +1,9 @@
 /******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
+This project will display random quotes and
+change colors with them.
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
-
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
 
 // This is an array of objects contating quotes that will be displayed in UI
 
@@ -21,29 +11,13 @@ var quotes = [
     {quote: '"Nobody knows what it means, it\'s provocative it gets the people going!"', source: 'Will Ferrell', citation: 'Blades of Glory'},
     {quote: '"I have a dream!"', source: 'Martin Luther King Jr.', year: 1963},
     {quote: '“You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.”', source: 'Dr. Suess'},
-    {quote: '“When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.”', source: 'Helen Keller'},
+    {quote: '“When one door of happiness closes, another opens.”', source: 'Helen Keller'},
     {quote: '“Great minds discuss ideas; average minds discuss events; small minds discuss people.”', source: 'Eleanor Roosevelt'}
 ];
 
-/*
-console.log(quotes);
-console.log(quotes[0].quote);
-*/
 
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
-//var message = '';
-/*
-function print(message) {
-  var outputDiv = document.getElementById('quote');
-  outputDiv.innerHTML = message;
-}
-*/
 
-// This is working
+// This function is generating a random number to help with indexing the quotes array of objects
 
 function getRandomQuote(){
     var randomNumber = Math.floor(Math.random() * quotes.length);
@@ -51,23 +25,8 @@ function getRandomQuote(){
 }
 
 
-//print(message);
 
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
-// Need to figure out how to show the qoutes[quoteIndex].quote on UI
+// This function is generating HTML for the random quote that will be selected
 
 function printQuote() {
     var quoteIndex = getRandomQuote();
@@ -88,25 +47,9 @@ function printQuote() {
     document.getElementById("quote-box").innerHTML = HTML;
 }
 
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-document.getElementById('loadQuote').addEventListener("click", changeColor, false);
 
-// Need to account for if button was hit first
-window.addEventListener("load", function() {
-    setInterval(function () {
-    changeColor();
-    printQuote();
-}, 15000); 
-});
 
 /*
     Function to change color when new quote is selected 
@@ -123,5 +66,4 @@ function changeColor() {
   i = (i + 1) % colors.length;
 }
 
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+document.getElementById('loadQuote').addEventListener("click", changeColor, false);
